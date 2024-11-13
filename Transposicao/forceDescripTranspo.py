4ab99431f76ab0418c50bf19b738373bc3b60d9d
JeanKaria <102553889+Jibabk@users.noreply.github.com>
2024-11-13 00:43:40 -0300

    Aprimoramento do comentarios
    Melhora dos comentários do código

--- Transposicao/forceDescripTranspo.py
+++ Transposicao/forceDescripTranspo.py
@@ -15,6 +15,7 @@ def verify(resposta):
             continue                    
     return chance
 
+#Pega os dividendos menores que 9 de um número
 def dividendos(n):
     retorno = []
     for i in range(1, n):
@@ -24,6 +25,7 @@ def dividendos(n):
             retorno.append(i)
     return retorno
 
+#Pega todas as permutações para uma lista de [0..n] elementos
 def permutacoes(n):
     return (list(itertools.permutations(list(range(n)))))
  
@@ -44,23 +46,32 @@ for i in range(len(digrafos)):
 #define alfabeto e frequencia das letras
 alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+#separa a cifra em uma lista de caracteres
 cifra = []
 for char in cypher:
     cifra.append(char.upper())
 
+#pega os divisores do tamanho da cifra
 final =[]
 order = defaultdict(list)
 divisores = dividendos(len(cifra))
+
+# para cada divisor se obtem o resultado mais provável
 for div in divisores:
+
+    #separa a cifra em uma quantidade de colunas igual ao divisor
     order.clear()
     for i in range(len(cifra)):
         order[(i//(len(cifra)//div))].append(cifra[i])
     perm = permutacoes(div)
     frase = []
+
+    #Para cada permutação possível é calculado a probabilidade de ser uma frase em portugues
     for p in perm:
         for tentativa in p:
             frase.append("".join(order[tentativa]))
 
+         #monta a resposta final e adciona ela a uma lista contendo a resposta mais provavel para cada divisor
         resp = ""
         for i in range(len(frase[0])):
             for j in range(div):
@@ -69,7 +80,7 @@ for div in divisores:
         frase = []
 
 
-#verifica qual a frase mais provavel
+#compara os resultados de cada tamanho de divisor e escolhe o maior
 maior = 0
 for tentativa in final:
     if verify(tentativa) > maior:
